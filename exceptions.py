69ffad2289ef41aa879df31123edcece633d61d1
Владимир Матасов <dragon111@mail.ru>
2024-01-18 12:00:18 +0300

    Correct programm after tests

---
+++ exceptions.py
@@ -0,0 +1,4 @@
+class AnotherCodeResponse(Exception):
+    """Класс исключения при возвращении кода ответа, отличного от 200."""
+
+    pass
--- homework.py
+++ homework.py
@@ -4,9 +4,11 @@ import logging
 import os
 import requests
 import sys
-from time import time, sleep
+import time
 
-from telegram import Bot
+import telegram
+
+from exceptions import AnotherCodeResponse
 
 
 load_dotenv()
@@ -25,10 +27,10 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-MESSAGE_ERROR_CHECK_TOKENS = '''
-Отсутствует обязательная переменная окружения: '{token}}'
-Программа принудительно остановлена.
-'''
+MESSAGE_ERROR_CHECK_TOKENS = (
+    'Отсутствует обязательная переменная окружения: {token}\n'
+    'Программа принудительно остановлена.'
+)
 
 
 logger = logging.getLogger(__name__)
@@ -40,41 +42,34 @@ handler.setFormatter(logging.Formatter(
 logger.addHandler(handler)
 
 
-def check_tokens() -> None:
-    """Функция проверки получения всех необходимых токенов."""
-    if PRACTICUM_TOKEN == '':
-        logger.critical(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=PRACTICUM_TOKEN)
-        )
-        raise SystemExit(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=PRACTICUM_TOKEN)
-        )
-    if TELEGRAM_TOKEN == '':
-        logger.critical(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=TELEGRAM_TOKEN)
-        )
-        raise SystemExit(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=TELEGRAM_TOKEN)
-        )
-    if TELEGRAM_CHAT_ID == '':
-        logger.critical(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=TELEGRAM_CHAT_ID)
-        )
-        raise SystemExit(
-            MESSAGE_ERROR_CHECK_TOKENS.format(token=TELEGRAM_CHAT_ID)
-        )
+def check_tokens():
+    """Функция проверки наличия необходимых токенов."""
+    tokens_inserts = (
+        (PRACTICUM_TOKEN, 'PRACTICUM_TOKEN'),
+        (TELEGRAM_TOKEN, 'TELEGRAM_TOKEN'),
+        (TELEGRAM_CHAT_ID, 'TELEGRAM_CHAT_ID'),
+    )
+
+    for token, insert in tokens_inserts:
+        if token is None:
+            logger.critical(
+                MESSAGE_ERROR_CHECK_TOKENS.format(token=insert)
+            )
+            return False
+    return True
 
 
 def send_message(bot, message):
     """Функция отправки сообщения в чат Telegram."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.debug(f'Бот отправил сообщение "{message}"')
-    except Exception as error:
+    except BaseException as error:
         logger.error(
             f'Ошибка при отправке ботом сообщения:\n{error}',
             exc_info=True
         )
+    else:
+        logger.debug(f'Бот отправил сообщение "{message}"')
 
 
 def get_api_answer(timestamp):
@@ -86,114 +81,115 @@ def get_api_answer(timestamp):
             params={'from_date': timestamp}
         )
 
-        if response.status_code == HTTPStatus.OK:
-            logger.info('Получен ответ "OK" от API (код 200).')
-            return response.json()
+        if response.status_code != HTTPStatus.OK:
+            message = (
+                'Сбой в работе программы: '
+                f'код ответа API {response.status_code}!'
+            )
+            logger.error(message)
+            raise AnotherCodeResponse(response.status_code)
 
-        message = '''
-Сбой в работе программы: код ответа API {code}!
-{addition}!
-'''
-        statuses_messages = {
-            '400': message.format(
-                code=400,
-                addition='Проверьте значение параметра "from_date"'
-            ),
-            '401': message.format(
-                code=401,
-                addition='Проверьте действительность и корректность токенов'
-            ),
-            '404': message.format(
-                code=404,
-                addition=f'Эндпоинт {ENDPOINT} недоступен!'
-            ),
-        }
-
-        if str(response.status_code) in statuses_messages:
-            logger.error(statuses_messages[str(response.status_code)])
-            return statuses_messages[str(response.status_code)]
-
+        logger.debug('Получен ответ "OK" от API (код 200).')
+        return response.json()
     except requests.ConnectionError as error:
-        message = f'''
-Сбой в работе программы: ошибка подключения к удаленному серверу!
-{error}
-'''
+        message = (
+            'Сбой в работе функции get_api_answer: '
+            f'ошибка подключения к удаленному серверу!\n{error}'
+        )
         logger.error(message, exc_info=True)
         return message
     except requests.HTTPError as error:
-        message = f'''
-Сбой в работе программы: ошибка подключения к удаленному серверу!
-{error}
-'''
+        message = (f'Сбой в работе программы: ошибка HTTP!\n{error}')
         logger.error(message, exc_info=True)
         return message
     except requests.URLRequired as error:
-        message = f'''
-Сбой в работе программы: для отправки запроса необходим действительный URL!
-{error}
-'''
+        message = (
+            'Сбой в работе функции get_api_answer: '
+            f'для отправки запроса необходим действительный URL!\n{error}'
+        )
         logger.error(message, exc_info=True)
         return message
     except requests.Timeout as error:
-        message = f'''
-Сбой в работе программы: время запроса истекло!
-{error}
-'''
+        message = (
+            'Сбой в работе функции get_api_answer: '
+            f'время запроса истекло!\n{error}'
+        )
         logger.error(message, exc_info=True)
         return message
     except requests.RequestException as error:
-        message = f'''
-Сбой в работе программы: неоднозначная ошибка при работе с запросом!
-{error}
-'''
+        message = (
+            'Сбой в работе функции get_api_answer: '
+            f'неоднозначная ошибка при работе с запросом!\n{error}'
+        )
         logger.error(message, exc_info=True)
         return message
 
 
 def check_response(response):
     """Функция проверки ответа API на соответствие документации."""
+    if type(response) is not dict:
+        logger.error('В ответе API данные поступили не в виде словаря!')
+        raise TypeError()
+    logger.debug('Данные в ответе API поступили в виде словаря.')
+
+    if "homeworks" not in response:
+        logger.error(
+            'В словаре данных из ответа API отсутвует ключ "homeworks"!'
+        )
+        raise KeyError()
+    logger.debug(
+        'В словаре данных из ответа API присутствует ключ "homeworks".'
+    )
+
+    if "current_date" not in response:
+        logger.error(
+            'В словаре данных из ответа API отсутвует ключ "current_date"!'
+        )
+        raise KeyError()
+    logger.debug(
+        'В словаре данных из ответа API присутствует ключ "current_date".'
+    )
+
+    if type(response["homeworks"]) is not list:
+        logger.error('Домашние работы поступают не списком!')
+        raise TypeError()
+    logger.debug('Домашние работы поступили списком.')
+    logger.debug('Основные поля в ответе API соответствуют документации.')
+
     try:
-        if "homeworks" in response and "current_date" in response:
-            message = 'Основные поля в ответе API соответствуют документации.'
-            logger.debug(message)
-            if len(response["homeworks"]) == 0:
-                logger.info('Новые статусы домашних работ отсутсвуют.')
-                return None
-            else:
-                return response["homeworks"]
-        else:
-            message = (
-                'Сбой в работе программы: '
-                'основные поля в ответе API не соответствуют документации.'
-            )
-            logger.error(message)
-            return message
+        if len(response["homeworks"]) == 0:
+            logger.info('Новые статусы домашних работ отсутсвуют.')
+        return response["homeworks"]
     except Exception as error:
-        message = f'''
-Сбой в работе программы: непредвиденная ошибка!
-{error}
-'''
+        message = (
+            'Сбой в работе функции check_response: '
+            f'непредвиденная ошибка!\n{error}'
+        )
         logger.error(message, exc_info=True)
     return message
 
 
 def parse_status(homework):
-    """Функция извлечения статуса конкретной домашней работы."""
+    """Функция извлечения статуса домашней работы."""
+    if "homework_name" not in homework:
+        logger.error(
+            'Ошибка данных: '
+            'в объекте "homework" отсутствует ключ "homework_name"!'
+        )
+        raise KeyError()
+    if "status" not in homework:
+        logger.error(
+            'Ошибка данных: '
+            'в объекте "homework" отсутствует ключ "status"!'
+        )
+        raise KeyError()
+    if homework["status"] not in HOMEWORK_VERDICTS:
+        logger.error(
+            f'Получен необычный статус домашней работы: {homework["status"]}'
+        )
+        raise KeyError()
+
     try:
-        if "homework_name" not in homework:
-            message = (
-                'Ошибка данных: '
-                'в объекте "homework" отсутствует ключ "homework_name"!'
-            )
-            logger.error(message)
-            return message
-        if "status" not in homework:
-            message = (
-                'Ошибка данных: '
-                'в объекте "homework" отсутствует ключ "homework_name"!'
-            )
-            logger.error(message)
-            return message
         message = (
             'Изменился статус проверки работы '
             f'"{homework["homework_name"]}". '
@@ -201,49 +197,45 @@ def parse_status(homework):
         )
         logger.info(message)
         return message
-    except KeyError():
+    except Exception as error:
         message = (
-            'Получен необычный статус домашней работы: '
-            f'{homework["status"]}'
+            'Сбой в работе функции parse_status: '
+            f'непредвиденная ошибка!\n{error}'
         )
-        logger.error(message)
-        return message
+        logger.error(message, exc_info=True)
+    return message
 
 
 def main():
     """Основная логика работы бота."""
-    check_tokens()
-    logger.debug('Токены проверены. Успех!')
-    bot = Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time())
+    if check_tokens():
+        logger.debug('Токены проверены. Успех!')
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    else:
+        logger.error(
+            'Ошибка запуска бота: не получены все необходимые токены!'
+        )
+        raise SystemExit()
+
+    timestamp = int(time.time())
 
     while True:
         try:
             logger.debug('Запрос отправлен.')
-            answer = get_api_answer(timestamp)
-            if type(answer) is str:
-                send_message(bot, answer)
-            else:
-                logger.debug('Ответ от API успешно получен.')
-                answer = check_response(answer)
-                if type(answer) is str:
-                    send_message(bot, answer)
-                elif type(answer) is dict:
-                    message = parse_status(answer)
-                    send_message(bot, message)
-                elif type(answer) is list:
-                    for homework in answer:
-                        message = parse_status(homework)
-                        send_message(bot, message)
-            sleep(RETRY_PERIOD)
+            response = get_api_answer(timestamp)
+            logger.debug('Ответ от API успешно получен.')
+            send_message(bot, 'Ответ от API успешно получен.')
+            homeworks = check_response(response)
+            for homework in homeworks:
+                message = parse_status(homework)
+                send_message(bot, message)
         except Exception as error:
-            message = f'''
-Сбой в работе программы: непредвиденная ошибка!
-{error}
-'''
-            logger.critical(message, exc_info=True)
+            message = (
+                f'Сбой в работе программы: непредвиденная ошибка!\n{error}'
+            )
+            logger.error(message, exc_info=True)
             send_message(bot, message)
-            raise SystemExit('Программа принудительно остановлена.')
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
